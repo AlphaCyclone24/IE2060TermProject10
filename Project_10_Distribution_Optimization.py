c170b8af2d1a05e7ed477787b62d40a23d761596
Snbark23 <Snbark23@gmail.com>
2024-04-03 09:21:37 -0500

    Fixed the Order Confirmation screen

--- Project_10_Distribution_Optimization.py
+++ Project_10_Distribution_Optimization.py
@@ -26,9 +26,9 @@ br = [124, 2, 68]
 no = [203, 75, 133]
 
 # Starts the application
-root = Tk()
-root.geometry("500x400")
-root.configure(background="#CF9FFF")
+order_screen = Tk()
+order_screen.geometry("500x400")
+order_screen.configure(background="#CF9FFF")
 
 # Get the selected date when the user closes the calendar
 def pick_date(event):
@@ -73,28 +73,12 @@ def TOD_change():
 def location_change():
     global location
     location = location_entry.get()
+    global text_location
+    text_location = location
 
 def amount_change():
     global amount
     amount = int(amount_entry.get())
-
-def confirmation_screen():
-    confirmation_window = Toplevel()
-    confirmation_window.geometry("500x400")
-    confirmation_window.configure(background="#CF9FFF")
-
-    confirmation_label = Label(confirmation_window, text=f"The date is {date}, the time is {hour}:{minute} {TOD} for {amount} material for ${final_price}", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
-    confirmation_label.place(x=10, y=198)
-
-def order_submission():
-    date_changed()
-    hour_changed()
-    minute_change()
-    location_change()
-    TOD_change()
-    amount_change()
-    print(f"The date is {date}, the time is {hour}:{minute} {TOD} for {amount} material")
-    confirmation_screen()
 
     monthdifference = abs(deliverymonth - currentmonth) * 30.4167
     daydifference = abs(deliveryday - currentday)
@@ -106,8 +90,8 @@ def order_submission():
         estimates = [estimatea, estimateb, estimatec]
         estimates = sorted(estimates)
         print(f"The cost is going to be {estimates[0]}")
-        global final_price
-        final_price = estimates[0]
+        global final_estimate
+        final_estimate = estimates[0]
 
     if "S" == location:
         global estimatea
@@ -173,25 +157,70 @@ def order_submission():
         print(f"The cost of storage c is {estimatec}")
         estimates()
 
+    def yes_button(event):
+        print("Successfully Submitted")
+
+    def no_button(event):
+        order_screen.window = Toplevel()
+
+def confirmation_screen():
+    confirmation_window = Toplevel()
+    confirmation_window.geometry("500x400")
+    confirmation_window.configure(background="#CF9FFF")
+
+    confirmation_date_label = Label(confirmation_window, text=f"The date is for delivery is {deliverymonth}/{deliveryday}/{deliveryyear}", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    confirmation_date_label.place(x=10, y=50)
+
+    confirmation_time_label = Label(confirmation_window, text=f"The time for the delivery is {hour}:{minute} {TOD}", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    confirmation_time_label.place(x=10, y=100)
+
+    confirmation_material_amount_label = Label(confirmation_window, text=f"The location to be delivered to is {text_location}", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    confirmation_material_amount_label.place(x=10, y=150)
+
+    confirmation_material_amount_label = Label(confirmation_window, text=f"The amount of material to be delivered is {amount}", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    confirmation_material_amount_label.place(x=10, y=200)
+
+    confirmation_price_amount_label = Label(confirmation_window, text=f"The estimated cost is ${final_estimate}", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    confirmation_price_amount_label.place(x=10, y=250)
+
+    confirmation_label = Label(confirmation_window, text=f"Is this information correct?", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    confirmation_label.place(x=10, y=300)
+
+    ttk.order_yes_submission_bttn = Button(confirmation_window, text="Yes", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    ttk.order_yes_submission_bttn.place(x=150, y=350)
+
+    ttk.order_no_submission_bttn = Button(confirmation_window, text="No", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+    ttk.order_no_submission_bttn.place(x=200, y=350)
+
+def order_submission():
+    date_changed()
+    hour_changed()
+    minute_change()
+    location_change()
+    TOD_change()
+    amount_change()
+    print(f"The date is {date}, the time is {hour}:{minute} {TOD} for {amount} material")
+    print(f"The cost is going to be {final_estimate}")
+    confirmation_screen()
 
 # Create Entry that will accept date of birth after clicking on submit
-date_label = Label(root, text="Date of Delivery: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+date_label = Label(order_screen, text="Date of Delivery: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
 date_label.place(x=10, y=158)
 
 # Set up for hour label on order screen
-hour_label = Label(root, text="Time of Delivery: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+hour_label = Label(order_screen, text="Time of Delivery: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
 hour_label.place(x=10, y=198)
 
 # Set up for location label on order screen
-location_label = Label(root, text="Location of Delivery: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+location_label = Label(order_screen, text="Location of Delivery: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
 location_label.place(x=10, y=238)
 
 # Set up for amount label on order screen
-amount_label = Label(root, text="Amount of Material: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+amount_label = Label(order_screen, text="Amount of Material: ", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
 amount_label.place(x=10, y=278)
 
 # Set up for date entry
-date_entry = Entry(root, highlightthickness=0, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui", 12, "bold"))
+date_entry = Entry(order_screen, highlightthickness=0, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui", 12, "bold"))
 date_entry.place(x=160, y=160, width=200)
 date_entry.insert(0, "dd/mm/yyy")
 date_entry.bind("<1>", pick_date)
@@ -213,11 +242,11 @@ location_entry = ttk.Combobox(state="readonly", value=["S", "LC", "LAF", "BR", "
 location_entry.place(x=185, y=245, width=50)
 
 # Creates a number input for amount of materials
-amount_entry = Entry(root, highlightthickness=0, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui", 12, "bold"))
+amount_entry = Entry(order_screen, highlightthickness=0, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui", 12, "bold"))
 amount_entry.place(x=185, y=280, width=50)
 
-ttk.order_sub_bttn = Button(root, command=order_submission, text="Order Submission", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
-ttk.order_sub_bttn.place(x=150, y=318)
+ttk.order_confirmation_bttn = Button(order_screen, command=order_submission, text="Order Confirmation", bg="#CF9FFF", fg="white", font=("yu gothic ui", 13, "bold"))
+ttk.order_confirmation_bttn.place(x=150, y=318)
 
-root.mainloop()
+order_screen.mainloop()
 
